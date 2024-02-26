a9b746644899c34a788060c871451d248aed25b9
Kugesz <kovacsgergo.algyo@gmail.com>
2024-02-26 13:30:13 +0100

    kész és klaszz

---
+++ app.js
@@ -0,0 +1,33 @@
+function Kereses(nev){
+    fetch(`https://hur.webmania.cc/zips/${nev}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => { 
+            Kiiras(data);
+        })
+        .catch(error => {
+            console.error('Error during fetch:', error);
+            return null;
+        });
+}
+
+function Kiiras(result){
+    container = document.getElementById("results");
+    console.log(result)
+    if(result.zips.length == 0){document.getElementById("error").textContent = "Az adatbázisunkban nem olt a kereséséenk megfelelő találat!"; return;}
+    result.zips.forEach(object =>{
+        console.log("halo")
+        container.innerHTML += `<p>${object.name}:  ${object.zip}</p>`
+    })
+}
+
+function Lekeres(){
+    document.getElementById("error").textContent = "";
+    nev = document.getElementById("nevInput").value;
+    console.log(nev);
+    Kereses(nev);
+}
